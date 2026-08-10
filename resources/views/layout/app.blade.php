c728939648d3fcdae4fabe14c651bfb7dd4d5b01
Syadam Radyan <101983260+AdamRadyan@users.noreply.github.com>
2026-08-10 18:50:26 +0700

    feat: initialize core layout, UI components, and build assets for cricket insight project

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -2,6 +2,16 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-M9JGWQ5B1Q"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-M9JGWQ5B1Q');
+    </script>
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title', config('app.name', 'Cricket Insight'))</title>
