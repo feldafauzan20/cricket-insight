e92e90af409aa241065e6ae62240c7208e34b468
feldafauzan20 <feldafauzan20@gmail.com>
2026-08-13 20:21:57 +0700

    feat: update gallery and magazine headers with localized content and dynamic links for livestreams

--- lang/en/gallery.php
+++ lang/en/gallery.php
@@ -1,6 +1,8 @@
 <?php
 
 return [
-    //
+    'gallery_header' => 'EXPLORE EVERY STORY THROUGH THE LENS',
+    'gallery_subheader' => 'Relive unforgettable cricket moments through curated photography',
+    'magazine_header' => 'CRICKET INSIGHT MAGAZINE',
+    'magazine_subheader' => 'Exclusive stories and in-depth perspectives from the world of Cricket Indonesia',
 ];
-
